3d1806c0f522da8d289f3e7576097f3455433fe7
ponjs <2552658135@qq.com>
2021-05-24 17:04:54 +0800

    🎈 perf: vuex modules namespaced

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,6 +1,6 @@
 import { Module, VuexModule } from 'vuex-module-decorators'
 
-@Module({ name: 'app' })
+@Module({ name: 'app', namespaced: true })
 export default class App extends VuexModule {
   name: string = process.env.VUE_APP_APP_NAME
 }
